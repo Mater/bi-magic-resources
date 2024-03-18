9efff3ac70cd679a13fa399a4f10101da46cc997
Yury Marmysh <yury.marmysh@instinctools.com>
2024-03-18 23:24:34 +0100

    fetch data

--- src/ds_12/OrdersBar.tsx
+++ src/ds_12/OrdersBar.tsx
@@ -30,18 +30,11 @@ const OrdersBar = ({ cfg: { dataSource } }: IVizelProps) => {
     KoobDataService,
     koob,
     dimensionDefs,
-    measuresDefs[0],
-    {},
-    2
+    measures.map((formula) => ({ formula })),
+    {}
   );
 
-  const dataSimple = [
-    { parameter: 'Параметр 1', number: 1234 },
-    { parameter: 'Параметр 2', number: 1083 },
-    { parameter: 'Параметр 3', number: 672 },
-    { parameter: 'Параметр 4', number: 301 },
-    { parameter: 'Параметр 5', number: 167 },
-  ];
+  console.log(values);
 
   return (
     <Bar
@@ -50,9 +43,9 @@ const OrdersBar = ({ cfg: { dataSource } }: IVizelProps) => {
         maxWidth: 300,
         maxHeight: 200,
       }}
-      data={dataSimple}
-      xField="number"
-      yField="parameter"
+      data={values}
+      xField="sum_unitcost"
+      yField="emp_fio"
     />
   );
 };
